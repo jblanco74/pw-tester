64ab54027f7f83965e81c649f136a402b7c3886d
Javier Blanco <jbits.solutions@gmail.com>
2022-05-17 12:14:47 +0100

    generate a random password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,13 +39,29 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    console.log(
-      `About to generate a password:
-      Includes Letters: ${this.includeLetters},
-      Includes Numbers: ${this.includeNumbers},
-      Includes Symbols: ${this.includeSymbols}`
-      );
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@?$%^&*()'
     
-    this.password = 'My password';
+    let validChars = '';
+    
+    if(this.includeLetters) {
+      validChars += letters;
+    }
+    if(this.includeNumbers) {
+      validChars += numbers;
+    }
+    if(this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.pwLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
   }
 }
